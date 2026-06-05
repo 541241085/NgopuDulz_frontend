62d09374146edd5b76b611c7e6ee238c8f38a51c
Hanna Pramesti Qaireen <hannapramestiqaireen10@gmail.com>
2026-06-05 07:51:12 +0700

    update kasir dan main xml

--- app/src/main/java/com/hanna/ngopidulz_frontend/MainActivity.kt
+++ app/src/main/java/com/hanna/ngopidulz_frontend/MainActivity.kt
@@ -10,26 +10,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 1. Kenalkan tombol-tombol navigasi
+        // Pastikan ID di bawah ini sama persis dengan yang ada di XML
         val btnNavEksplor = findViewById<LinearLayout>(R.id.btn_nav_eksplor)
         val btnNavPesanan = findViewById<LinearLayout>(R.id.btn_nav_pesanan)
         val btnNavProfil = findViewById<LinearLayout>(R.id.btn_nav_profil)
 
-        // 2. Beri fungsi klik (Intent) ke masing-class Activity
         btnNavEksplor.setOnClickListener {
             startActivity(Intent(this, EksplorActivity::class.java))
         }
 
         btnNavPesanan.setOnClickListener {
-            startActivity(Intent(this, RiwayatPesananActivity   ::class.java))
+            // Hapus spasi tambahan pada nama kelas
+            startActivity(Intent(this, RiwayatPesananActivity::class.java))
         }
 
         btnNavProfil.setOnClickListener {
-            // Pastikan nama file profil-mu adalah ProfileActivity (sesuaikan jika namanya beda)
             startActivity(Intent(this, ProfileActivity::class.java))
         }
-
-        // Catatan: Tombol "Beranda" tidak perlu dikasih fungsi klik,
-        // karena saat ini kita SUDAH BERADA di halaman Beranda (MainActivity).
     }
 }
